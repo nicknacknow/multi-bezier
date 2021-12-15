02101c4503597bd5dfa6c5411a09ba0e9008acd4
nicholas <54144689+nicknacknow@users.noreply.github.com>
2021-12-15 22:36:54 +0000

    Update bezier.lua
    specified stuff

--- bezier.lua
+++ bezier.lua
@@ -1,4 +1,4 @@
-local function multi_bezier(t, pos)
+local function multi_bezier(t, pos) -- pos is a table for vectors 
 	local x,y,z = 0,0,0
 	
 	for i = #pos - 1, 0, -1 do
@@ -7,7 +7,7 @@ local function multi_bezier(t, pos)
 		local brackets = (1-t)^i
 		local bracket_mult = ((i == 0 or i == #pos - 1) and 1 or #pos-1) * (t ^ (index - 1))
 		
-		x = x + bracket_mult * brackets * pos[index].X
+		x = x + bracket_mult * brackets * pos[index].X -- pos[index] is the vector at that index in the table, .X is for getting the x value of the vector
 		y = y + bracket_mult * brackets * pos[index].Y
 		z = z + bracket_mult * brackets * pos[index].Z -- remove z if only 2d
 	end
